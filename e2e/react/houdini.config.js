767d8857176c8c1d684c5adeefe0ff6aa7cf33f2
d2du <me@siddharthjain.in>
2025-10-16 10:28:30 +0530

    local version works all good

---
+++ e2e/react/houdini.config.js
@@ -0,0 +1,43 @@
+/// <references types="houdini-react">
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	include: ['src/{components,routes}/**/*.{ts,tsx,gql}'],
+  schemaPath: './schema.graphql',
+	defaultPartial: true,
+	scalars: {
+		DateTime: {
+			type: 'Date',
+			unmarshal(val) {
+				return new Date(val)
+			},
+			marshal(val) {
+				return val.getTime()
+			},
+		},
+		File: {
+			type: 'File',
+		},
+	},
+
+	types: {
+		RentedBook: {
+			keys: ['userId', 'bookId'],
+		},
+		Sponsor: {
+			keys: ['name'],
+		},
+	},
+
+	plugins: {
+		'houdini-react': {},
+	},
+
+	router: {
+		auth: {
+			redirect: '/auth/token',
+			sessionKeys: ['supersecret'],
+		},
+	},
+}
+
+export default config
